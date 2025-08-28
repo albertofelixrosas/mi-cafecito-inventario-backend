33c978036e2d6a24a1a97d8adc61ddcbc9af0656
Alberto Félix Rosas <alberto.felix.js@gmail.com>
2025-08-27 23:47:17 -0700

    feat: Add Product repository to ProductCategoriesService and implement validation for category removal

--- src/product-categories/product-categories.module.ts
+++ src/product-categories/product-categories.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductCategoriesService } from './product-categories.service';
 import { ProductCategoriesController } from './product-categories.controller';
 import { ProductCategory } from './entities/product-category.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductCategory])],
+  imports: [TypeOrmModule.forFeature([ProductCategory, Product])],
   controllers: [ProductCategoriesController],
   providers: [ProductCategoriesService],
   exports: [ProductCategoriesService],
--- src/product-categories/product-categories.service.ts
+++ src/product-categories/product-categories.service.ts
@@ -5,12 +5,15 @@ import { ProductCategory } from './entities/product-category.entity';
 import { CreateProductCategoryDto } from './dto/create-product-category.dto';
 import { UpdateProductCategoryDto } from './dto/update-product-category.dto';
 import { FilterProductCategoriesDto } from './dto/filter-product-categories.dt';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class ProductCategoriesService {
   constructor(
     @InjectRepository(ProductCategory)
     private readonly categoryRepository: Repository<ProductCategory>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
   ) {}
 
   async create(createDto: CreateProductCategoryDto): Promise<ProductCategory> {
@@ -58,6 +61,19 @@ export class ProductCategoriesService {
 
   async remove(id: number): Promise<void> {
     const category = await this.findOne(id);
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+    // Verificar si la categoría tiene productos asociados
+    const products = await this.productRepository.find({
+      where: { category: { productCategoryId: id } },
+    });
+
+    if (products.length > 0) {
+      throw new NotFoundException(
+        `No se puede eliminar la categoría "${category.productCategoryName}" por que tiene productos asociados`,
+      );
+    }
     await this.categoryRepository.remove(category);
   }
 }
